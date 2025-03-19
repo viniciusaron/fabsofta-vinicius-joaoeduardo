2a47343166ed7632612baa0113fab80b82dd52a3
JFormigoni <joaoeformigoni14@gmail.com>
2025-03-18 21:50:56 -0300

    feat: continuação revisão java

--- revisao_joaoeduardo/src/main/java/br/univille/App.java
+++ revisao_joaoeduardo/src/main/java/br/univille/App.java
@@ -1,11 +1,20 @@
 package br.univille;
+
+import br.univille.entity.Cidade;
+import br.univille.entity.Pessoa;
+
 public class App {
     public static void main(String[] args) throws Exception {
         //System.out.println("Hello, World!");
         //Instância
+        Cidade jlle = new Cidade();
+        jlle.setNome("Joinville");
+        jlle.setEstado("Santa Catarina");
+
         Pessoa fulano = new Pessoa("fulano");
         Pessoa zezinho = new Pessoa();
         zezinho.setNome("Zezinho");
+        zezinho.setCidade(jlle);
 
         System.out.println(fulano);
         System.out.println(zezinho);    
--- revisao_joaoeduardo/src/main/java/br/univille/entity/Pessoa.java
+++ revisao_joaoeduardo/src/main/java/br/univille/entity/Pessoa.java
@@ -1,14 +1,37 @@
-package br.univille;
+package br.univille.entity;
 public class Pessoa {
     // atributo (variável)
+    private long id;
     private String nome; 
+    private String endereco;
+
+    private Cidade cidade;
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 
     // construtor (mesmo nome da classe, não tem retorno)
     public Pessoa(String nome) {
         // this referencia a classe
         this.nome = nome;
     }
-
     // dois métodos com assinatura semelhante = polimorfismo
     public Pessoa() {
         
@@ -27,6 +50,7 @@ public class Pessoa {
     @Override
     public String toString() {
 
-        return super.toString() + getNome();
+        return getNome();
     }
+  
 }
