8d161e2f7cb20732c0284413a22c484b7afd9639
viniciusaron <viniciusarongg@gmail.com>
2025-04-01 22:01:56 -0300

    atualizações em código

---
+++ projfabsofttireshop/src/main/java/br/univille/projfabsofttireshop/ProjfabsofttireshopApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsofttireshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsofttireshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsofttireshopApplication.class, args);
+	}
+
+}
---
+++ projfabsofttireshop/src/test/java/br/univille/projfabsofttireshop/ProjfabsofttireshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsofttireshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsofttireshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
