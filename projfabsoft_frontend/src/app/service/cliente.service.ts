a55b48ebe27b8260eb8aec583ecee35a4fdcfde9
Vinicius Aron Patricio do Prado <viniciusarongg@gmail.com>
2025-06-04 23:21:26 +0000

    criação funcionario novo e novas classes component

--- projfabsoft_frontend/src/app/service/cliente.service.ts
+++ projfabsoft_frontend/src/app/service/cliente.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../model/cliente';
-import { HttpClient } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-  apiURL = "http://localhost:8080/api/v1/clientes"
+  apiURL = "http://localhost:8080/api/v1/clientes";
+
   constructor(private http:HttpClient) { }
 
   getClientes(){
-    return this.http.get<Cliente[]>(this.apiURL);
+     return this.http.get<Cliente[]>(this.apiURL);
+  }
+  saveCliente(cliente:Cliente){
+    return this.http.post(this.apiURL,cliente);
   }
 }
