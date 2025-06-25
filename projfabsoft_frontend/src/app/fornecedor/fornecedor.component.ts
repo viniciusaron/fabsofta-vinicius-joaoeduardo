410dc8b3d16bad9a445fff0a30b0aba224a2290f
Vinicius Aron Patricio do Prado <viniciusarongg@gmail.com>
2025-06-25 00:58:49 +0000

    termino classe fornecedor

--- projfabsoft_frontend/src/app/fornecedor/fornecedor.component.ts
+++ projfabsoft_frontend/src/app/fornecedor/fornecedor.component.ts
@@ -1,28 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Fornecedor } from '../model/fornecedor';
 import { FornecedorService } from '../service/fornecedor.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-fornecedor',
-  imports: [HttpClientModule, CommonModule, RouterLink],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './fornecedor.component.html',
   styleUrl: './fornecedor.component.css',
-  providers: [FornecedorService]
+  providers: [FornecedorService, Router]
 })
 export class FornecedorComponent {
 
-    public listaFornecedores:Fornecedor[] = [];
-    
-    constructor(
-      private fornecedorService:FornecedorService
-    ){}
+  public listaFornecedores:Fornecedor[] = [];
 
-    ngOnInit(): void {
-      this.fornecedorService.getClientes().subscribe(resposta => {
-          this.listaFornecedores = resposta;
-      })
-    }
+  @ViewChild('myModal') modalElement!: ElementRef;
+  private modal!: bootstrap.Modal;
+
+  private fornecedorSelecionado!: Fornecedor;
+  
+  constructor(
+    private fornecedorService:FornecedorService,
+    private router:Router
+  ){}
+
+  ngOnInit(): void{
+    this.fornecedorService.getFornecedores().subscribe(resposta => {
+      this.listaFornecedores = resposta;
+    })
+  }
+  novo(){
+    this.router.navigate(['fornecedores/novo']);
+  }
+  alterar(fornecedor:Fornecedor){
+    this.router.navigate(['fornecedores/alterar', fornecedor.id]);
+  }
+
+  abrirConfirmacao(fornecedor:Fornecedor) {
+    this.fornecedorSelecionado = fornecedor;
+    this.modal = new bootstrap.Modal(this.modalElement.nativeElement);
+    this.modal.show();
+  }
+
+  fecharConfirmacao() {
+    this.modal.hide();
+  }
+
+  confirmarExclusao() {
+    this.fornecedorService.excluirFornecedor(this.fornecedorSelecionado.id).subscribe(
+        () => {
+            this.fecharConfirmacao();
+            this.fornecedorService.getFornecedores().subscribe(
+              fornecedores => {
+                this.listaFornecedores = fornecedores;
+              }
+            );
+        },
+        error => {
+            console.error('Erro ao excluir fornecedor:', error);
+        }
+    );
+  }
+
 }
--- projfabsoft_frontend/src/app/service/fornecedor.service.ts
+++ projfabsoft_frontend/src/app/service/fornecedor.service.ts
@@ -2,16 +2,28 @@ import { Injectable } from '@angular/core';
 import { Fornecedor } from '../model/fornecedor';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class ClienteFornecedor {
-  apiURL = "http://localhost:8080/api/v1/fornecedor";
-  
+export class FornecedorService {
+  apiURL = "http://localhost:8080/api/v1/fornecedores";
+
   constructor(private http:HttpClient) { }
 
   getFornecedores(){
     return this.http.get<Fornecedor[]>(this.apiURL);
   }
-
+  saveFornecedor (fornecedor:Fornecedor){
+    if(fornecedor.id){
+      return this.http.put(this.apiURL + '/' + fornecedor.id, fornecedor);
+    }
+    return this.http.post(this.apiURL,fornecedor);
+  }
+  getFornecedorById(id: any) {
+    return this.http.get<Fornecedor>(this.apiURL + '/' + id);
+  }
+  excluirFornecedor(id: any){
+    return this.http.delete<Fornecedor>(this.apiURL + '/' + id);
+  }
 }
